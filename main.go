58c6b7efddd3a7b3913b16c9efbacf53734850f1
Brandon Hsiao <bh@codeperfect95.com>
2021-10-19 19:59:18 -0700

    save changes

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ import (
 
 var flagWindowId = flag.String("windowid", "", "window id")
 var flagDelay = flag.Int("delay", 250, "delay between frames in milliseconds")
+var flagOutputFolder = flag.String("output", "", "output folder")
 
 func takeScreenshots(stop chan bool, ssfolder string) {
 	for {
@@ -87,6 +88,28 @@ type Rect struct {
 	X2 int
 }
 
+func expandRect(r *Rect, w, h, delta int) {
+	r.X1 -= delta
+	if r.X1 < 0 {
+		r.X1 = 0
+	}
+
+	r.Y1 -= delta
+	if r.Y1 < 0 {
+		r.Y1 = 0
+	}
+
+	r.X2 += delta
+	if r.X2 >= w {
+		r.X2 = w - 1
+	}
+
+	r.Y2 += delta
+	if r.Y2 >= h {
+		r.Y2 = h - 1
+	}
+}
+
 func diff(a, b Image) []Rect {
 	if len(a) != len(b) || len(a[0]) != len(b[0]) {
 		log.Fatalf(
@@ -131,16 +154,29 @@ func diff(a, b Image) []Rect {
 		}
 		seen[y][x] = true
 
-		dy := []int{1, -1, 0, 0}
-		dx := []int{0, 0, 1, -1}
-		for i := 0; i < len(dy); i++ {
-			x2, y2 := x+dx[i], y+dy[i]
-			if 0 <= x2 && x2 < w && 0 <= y2 && y2 < h {
-				if areDifferent(x2, y2) && !seen[y2][x2] {
-					floodfill(x2, y2, r)
+		// dy := []int{1, -1, 0, 0}
+		// dx := []int{0, 0, 1, -1}
+
+		for x2 := x - 3; x2 <= x+3; x2++ {
+			for y2 := y - 3; y2 <= y+3; y2++ {
+				if 0 <= x2 && x2 < w && 0 <= y2 && y2 < h {
+					if areDifferent(x2, y2) && !seen[y2][x2] {
+						floodfill(x2, y2, r)
+					}
 				}
 			}
 		}
+
+		/*
+			for i := 0; i < len(dy); i++ {
+				x2, y2 := x+dx[i], y+dy[i]
+				if 0 <= x2 && x2 < w && 0 <= y2 && y2 < h {
+					if areDifferent(x2, y2) && !seen[y2][x2] {
+						floodfill(x2, y2, r)
+					}
+				}
+			}
+		*/
 	}
 
 	rects := []Rect{}
@@ -149,6 +185,7 @@ func diff(a, b Image) []Rect {
 			if areDifferent(x, y) && !seen[y][x] {
 				r := Rect{Y1: y, X1: x, Y2: y, X2: x}
 				floodfill(x, y, &r)
+				expandRect(&r, w, h, 10)
 				rects = append(rects, r)
 			}
 		}
@@ -228,6 +265,12 @@ func main() {
 	if *flagWindowId == "" {
 		log.Fatalf("-windowid is required")
 	}
+	if *flagDelay == 0 {
+		log.Fatalf("-delay is required and can't be 0")
+	}
+	if *flagOutputFolder == "" {
+		log.Fatalf("-output is required and can't be 0")
+	}
 
 	ssfolder, err := ioutil.TempDir("", "cpcast_screenshots")
 	fmt.Printf("%s\n", ssfolder)
@@ -289,11 +332,11 @@ func main() {
 	}
 
 	type FrameInfo struct {
-		Timestamp int      `json:"timestamp"`
-		Changes   []Change `json:"changes"`
+		Timestamp int       `json:"timestamp"`
+		Changes   []*Change `json:"changes"`
 	}
 
-	frames := []FrameInfo{}
+	frames := []*FrameInfo{}
 
 	var lastFrame [][]Pixel
 	for i, file := range allFiles {
@@ -311,25 +354,28 @@ func main() {
 			diffRegions = []Rect{region}
 		} else {
 			diffRegions = diff(frame, lastFrame)
-			if len(diffRegions) > 50 {
-				// if we have a fuckload of tiny regions, just combine into one region
-				superRegion := diffRegions[0]
-				for _, region := range diffRegions {
-					if region.X1 < superRegion.X1 {
-						superRegion.X1 = region.X1
+			/*
+				if len(diffRegions) > 50 {
+					// if we have a fuckload of tiny regions, just combine into one region
+					superRegion := diffRegions[0]
+					for _, region := range diffRegions {
+						if region.X1 < superRegion.X1 {
+							superRegion.X1 = region.X1
+						}
+						if region.X2 > superRegion.X2 {
+							superRegion.X2 = region.X2
+						}
+						if region.Y1 < superRegion.Y1 {
+							superRegion.Y1 = region.Y1
+						}
+						if region.Y2 > superRegion.Y2 {
+							superRegion.Y2 = region.Y2
+						}
 					}
-					if region.X2 > superRegion.X2 {
-						superRegion.X2 = region.X2
-					}
-					if region.Y1 < superRegion.Y1 {
-						superRegion.Y1 = region.Y1
-					}
-					if region.Y2 > superRegion.Y2 {
-						superRegion.Y2 = region.Y2
-					}
+					expandRect(&superRegion, len(frame[0]), len(frame), 10)
+					diffRegions = []Rect{superRegion}
 				}
-				diffRegions = []Rect{superRegion}
-			}
+			*/
 		}
 
 		lastFrame = frame
@@ -340,7 +386,10 @@ func main() {
 
 		log.Printf("found %d regions", len(diffRegions))
 
-		frameInfo := FrameInfo{}
+		frameInfo := &FrameInfo{
+			Timestamp: file.Timestamp,
+		}
+
 		for i, region := range diffRegions {
 			// print out first 20 regions to get a sense
 			if i < 20 {
@@ -350,7 +399,7 @@ func main() {
 				)
 			}
 
-			frameInfo.Changes = append(frameInfo.Changes, Change{
+			frameInfo.Changes = append(frameInfo.Changes, &Change{
 				X:  region.X1,
 				Y:  region.Y1,
 				ID: len(images),
@@ -369,7 +418,7 @@ func main() {
 		frames = append(frames, frameInfo)
 	}
 
-	if err := os.MkdirAll("output", 0700); err != nil {
+	if err := os.MkdirAll(*flagOutputFolder, 0700); err != nil {
 		log.Fatal(err)
 	}
 
@@ -380,13 +429,18 @@ func main() {
 		Sprites: []*Sprite{},
 	}
 
-	if err := ip.CreateImage("output/spritesheet.png"); err != nil {
+	if err := ip.CreateImage(path.Join(*flagOutputFolder, "spritesheet.png")); err != nil {
 		log.Fatal(err)
+	}
+
+	spritesByID := map[int]*Sprite{}
+	for _, sprite := range ip.Sprites {
+		spritesByID[sprite.ID] = sprite
 	}
 
 	for _, frame := range frames {
 		for _, change := range frame.Changes {
-			sprite := ip.Sprites[change.ID]
+			sprite := spritesByID[change.ID]
 			change.X1 = sprite.X1
 			change.Y1 = sprite.Y1
 			change.X2 = sprite.X2
@@ -394,12 +448,36 @@ func main() {
 		}
 	}
 
-	data, err := json.Marshal(frames)
+	// write frames into compressed format
+
+	jsonframes := []interface{}{}
+	for _, frame := range frames {
+		jsonchanges := [][]int{}
+		for _, change := range frame.Changes {
+			jsonchange := []int{
+				change.X,
+				change.Y,
+				change.X1,
+				change.Y1,
+				change.X2,
+				change.Y2,
+			}
+			jsonchanges = append(jsonchanges, jsonchange)
+		}
+
+		jsonframe := []interface{}{
+			frame.Timestamp,
+			jsonchanges,
+		}
+		jsonframes = append(jsonframes, jsonframe)
+	}
+
+	data, err := json.Marshal(jsonframes)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := ioutil.WriteFile("output/data.json", data, 0644); err != nil {
+	if err := ioutil.WriteFile(path.Join(*flagOutputFolder, "data.json"), data, 0644); err != nil {
 		log.Fatal(err)
 	}
 
